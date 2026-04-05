890f4abe293f40f4c03e64eb85335329d821937f
Yuma-E17 <yumaisawesome1@gmail.com>
2026-04-05 17:52:49 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/My-Entertainment-App/sw.js', { scope: '/My-Entertainment-App/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
